b94a35f57b03aa799eff9d186cca2dbe7ce84da6
Davi Damasceno Cavalcante <venum.davi@gmail.com>
2023-11-10 11:43:14 -0300

    if else

--- JavaScript/3_ESTRUTURAS/script.js
+++ JavaScript/3_ESTRUTURAS/script.js
@@ -27,3 +27,85 @@ let $teste = "Ok";
 console.log(_teste);
 console.log($teste);
 
+// 3 - prompt
+// const age = prompt("Digite a sua idade: ");
+
+// console.log (`Você tem ${age} anos.`);
+
+// 4 - alert
+// alert("Testando!");
+
+// const z = 10;
+// alert(`${z}`)
+
+// 5 - Math
+// console.log(Math.max(5, 2, 1, 10));
+
+// console.log(Math.floor(5.14));    
+
+// console.log(Math.ceil(5.14));   
+
+// // 6 - console
+// console.log("Teste!")
+
+// console.error("Erro!")
+
+// console.warn("Aviso!")
+
+// 7 - if
+const m = 10;
+
+if (m > 5) {
+    console.log("m é maior que 5");
+}
+
+const user = "João"
+
+if (user === "João") {
+    console.log("Olá João!");
+}
+
+if (user === "Maria") {
+    console.log("Olá Maria!");
+}
+
+console.log(user === "João", user === "Maria");
+
+// 8 - else
+const loggedIn = false
+
+if (loggedIn) {
+    console.log("Está autenticado!");
+} else {
+    console.log("Não está autenticado!");
+}
+
+const q = 10;
+const w = 15;
+
+if (q > 5 && w > 20) {
+    console.log("Números mais altos");
+} else {
+    console.log("Os números não são mais altos");
+}
+
+// 9 - else if
+if (1 > 2) {
+    console.log("Teste!");
+} else if (2 > 3) {
+    console.log("Teste 2!");
+} else if (5 > 1) {
+    console.log("Agora sim!");
+}
+
+const userName = "Davi"
+const userAge = 23
+
+if(userName === "Diogo") {
+    console.log("Bem vindo Diogo!");
+} else if (userName === "Davi" && userAge === 23) {
+    console.log("Olá Davi, você tem 23 anos!");
+} else {
+    console.log("Nenhuma confição aceita!");
+}
+
