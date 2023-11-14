e9ac635318ddf9d8688760c6129a4700a9faf2b4
Davi Damasceno Cavalcante <venum.davi@gmail.com>
2023-11-14 11:29:40 -0300

    loop

--- JavaScript/3_ESTRUTURAS/script.js
+++ JavaScript/3_ESTRUTURAS/script.js
@@ -109,3 +109,19 @@ if(userName === "Diogo") {
     console.log("Nenhuma confição aceita!");
 }
 
+// 10 - While
+
+let p = 0;
+
+while(p<5) {
+    console.log(`Repetindo ${p}`);
+    p = p+1;
+}
+
+// Loop infinito
+// let x=10
+
+// while(x > 5) {
+//     console.log(`Imprimindo ${x}`)
+// }
+
