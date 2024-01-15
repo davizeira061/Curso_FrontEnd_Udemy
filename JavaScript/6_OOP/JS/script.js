3ab648f21d06a7d0995a0745f4d7c1f42b86adf6
Davi Damasceno Cavalcante <107430215+davizeira061@users.noreply.github.com>
2024-01-15 15:01:26 -0300

    finalizando oop

--- JavaScript/6_OOP/JS/script.js
+++ JavaScript/6_OOP/JS/script.js
@@ -74,6 +74,106 @@ console.log(ragnar);
 const azula = criarCachorro("Azula", "Vira-lata");
 console.log(azula);
 
+// 7 - funções como classe
+function Cachorro(nome, raca) {
+    this.nome = nome;
+    this.raca = raca
+}
+
+const husky = new Cachorro("Bob", "Husky");
+
+console.log(husky);
+
+//  8 - metodos na função construtora
+Cachorro.prototype.uivar = function () {
+    console.log("auuuuu!");
+}
+console.log(Cachorro.prototype);
+husky.uivar();
+
+// 9 - classe ES6
+class CachorroClasse {
+    constructor(nome, raca) {
+        this.nome = nome;
+        this.raca = raca;
+    }
+}
+
+const spyke = new CachorroClasse("Spyke", "Labrador");
+console.log(spyke);
+console.log(Object(spyke));
+
+// 10 - Mais sobre classes
+class Caminhao {
+    constructor(eixos, cor) {
+        this.eixos = eixos;
+        this.cor = cor;
+    }
+
+    descreverCaminhao(){
+        console.log(`Este caminhão tem ${this.eixos} e é da cor ${this.cor}`);
+    }
+}
+const scania = new Caminhao(6, "Vermelho")
+console.log(scania);
+scania.descreverCaminhao();
+Caminhao.motor = 4;
+const c2 = new Caminhao(4, "Preta");
+console.log(c2);
+console.log(c2.motor);
+Caminhao.prototype.motor = 4.0;
+const c3 = new Caminhao(6, "Azul");
+console.log(c3.motor);
+
+// 11 - get setter
+
+// 12 - herança
+class Mamifero{
+    constructor(patas) {
+        this.patas = patas;
+    }
+}
+
+class Lobo extends Mamifero {
+    constructor(patas, nome) {
+        super(patas, patas);
+        this.nome = nome;
+    }
+}
+
+const shark = new Lobo(4, "Shark");
+console.log(shark);
+console.log(shark.patas);
+
+// 13 - Instanceof
+console.log(shark instanceof Lobo);
+console.log(Lobo instanceof Mamifero);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ JavaScript/7_DEBUG/script/script.js
@@ -0,0 +1,107 @@
+// 1 - strict
+"use strict";
+
+// opa = "teste";
+const opa = "teste";
+
+// 2 - console.log
+let a = 1;
+let b = 2;
+
+if(a == 1) {
+    a = b + 2
+}
+
+// 3 - debugger
+let c = 1;
+let d = 2;
+
+if (c == 1) {
+    c = d + 2;
+}
+
+// debugger; 
+
+for(let i = 0; i < d; i++) {
+    c = c + 2;
+}
+
+console.log("Executou o loop");
+
+// debugger;
+
+if (c > 5) {
+    c = 25;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
