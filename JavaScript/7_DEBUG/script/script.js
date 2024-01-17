0e2858ce47e6601404ca4c255234d62cbdbb451b
Davi Damasceno Cavalcante <107430215+davizeira061@users.noreply.github.com>
2024-01-17 14:23:39 -0300

    debug finalizado

--- JavaScript/7_DEBUG/script/script.js
+++ JavaScript/7_DEBUG/script/script.js
@@ -8,7 +8,7 @@ const opa = "teste";
 let a = 1;
 let b = 2;
 
-if(a == 1) {
+if (a == 1) {
     a = b + 2
 }
 
@@ -22,7 +22,7 @@ if (c == 1) {
 
 // debugger; 
 
-for(let i = 0; i < d; i++) {
+for (let i = 0; i < d; i++) {
     c = c + 2;
 }
 
@@ -34,13 +34,60 @@ if (c > 5) {
     c = 25;
 }
 
+// 4 - tratamento de dados
+function checkNumber(n) {
+    const result = Number(n)
 
+    if (Number.isNaN(result)) {
+        console.log("Valor incorreto!");
+        return;
+    }
 
+    console.log("Valor correto!");
+    return result;
+}
 
+checkNumber(5);
+checkNumber("kkk");
+checkNumber({});
+checkNumber("7");
 
+// 5 - exceptions
+let x = 11;
 
+if (x != 11) {
+    throw new Error("O valor de x não pode ser diferente de 11!")
+}
 
+// 6 - Try catch
+try {
+    const soma = x + y;
+} catch (error) {
+    console.log(`Erro no programa: ${error}`)
+}
 
+// 7 - finaly
+try {
+    const value = checkNumber("")
+
+    if (!value) {
+        throw new Error("Valores inválidos")
+    }
+} catch (error) {
+    console.log(`Opa, aconteceu um problema: ${error}`);
+} finally {
+    console.log("O código foi executado!");
+}
+
+// 8 - assertion
+function checkNumber(arr) {
+    if (arr.length === 0) {
+        throw new Error("O array precisa ter elementos");
+    } else {
+        console.log(`O array tem ${arr.length} elementos`);
+    }
+}
+ checkNumber([1, 2, 3, 4, 5]);
 
 
 
