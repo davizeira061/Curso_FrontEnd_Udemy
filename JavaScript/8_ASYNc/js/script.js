687f231772657e3ba945c0eaf95940fe54236ace
Davi Damasceno Cavalcante <107430215+davizeira061@users.noreply.github.com>
2024-01-17 14:57:30 -0300

    assincrionas

---
+++ JavaScript/8_ASYNc/js/script.js
@@ -0,0 +1,20 @@
+// 1 - setTimeout
+console.log("Ainda não executou");
+
+setTimeout(function () {
+    console.log("Requisição assíncrona");
+}, 5000)
+console.log("Ainda não executou 2");
+
+// 2 - setInterval
+// console.log("Ainda não começou");
+
+// setInterval(function(){
+//     console.log("Intervalo assíncrono")
+// }, 5000);
+// console.log("Ainda não começou 2");
+
+
+
+
+
