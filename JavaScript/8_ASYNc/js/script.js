eb9405abe751519c3f5752d8ddc923226de21383
Davi Damasceno Cavalcante <107430215+davizeira061@users.noreply.github.com>
2024-01-18 14:56:30 -0300

    Update script.js

--- JavaScript/8_ASYNc/js/script.js
+++ JavaScript/8_ASYNc/js/script.js
@@ -14,6 +14,31 @@ console.log("Ainda não executou 2");
 // }, 5000);
 // console.log("Ainda não começou 2");
 
+// 3 - promises
+const promessa = Promise.resolve(5 + 5);
+console.log("teste");
+
+promessa.then(value => {
+    console.log(`A soma é ${value}`)
+    return value;
+})
+    .then((value) => value - 1)
+    .then((value) => console.log(`Agora é ${value}`));
+
+console.log("teste");
+
+// 4 - falha na promise
+Promise.resolve(4 * "asd")
+    .then((n) => {
+        if (Number.isNaN(n)) {
+            throw new Error("Valores inválidos");
+        }
+    })
+    .catch((err) => console.log(`Um erro ocorreu: ${err}`));
+
+
+
+
 
 
 
