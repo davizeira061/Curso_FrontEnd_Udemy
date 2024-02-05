5ed54375d4ff138b6dc59dc9b9c8995884185aac
Davi Damasceno Cavalcante <107430215+davizeira061@users.noreply.github.com>
2024-02-05 14:10:36 -0300

    update

--- projetos-do-curso/projeto6_tabuada/script.js
+++ projetos-do-curso/projeto6_tabuada/script.js
@@ -1,6 +1,29 @@
 // Seleção de elementos
-const multiplicationForm = document.querySelector("#multiplication-Form")
+const multiplicationForm = document.querySelector(".multiplication-form");
+const numberInput = document.querySelector("#number");
+const multiplicationInput = document.querySelector("#multiplicator");
+
+const multiplicationTable = document.querySelector("#multiplication-operations");
 
 // Funções
+const createTable = (number, multiplicatorNumber) => {
+    multiplicationTable.innerHTML = "";
+
+    for (i = 1; i <= multiplicatorNumber; i++) {
+        const result = number * i;
+        console.log(result);
+    }
+};
 
 // Eventos
+multiplicationForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const multiplicationNumber = +numberInput.value;
+
+    const multiplicatorNumber = +multiplicationInput.value;
+
+    if (!multiplicationNumber || !multiplicatorNumber) return;
+
+    console.log(multiplicationNumber, multiplicatorNumber);
+});
